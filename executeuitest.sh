e4c98e9a824f39a4bace8fe840e178a0ea8f53d1
Jayasankar <jayasankar.m@gmail.com>
2019-09-20 00:02:18 +0530

    file converted to unix format

--- executeuitest.sh
+++ executeuitest.sh
@@ -1,6 +1,6 @@
 echo $PWD
 echo `ls`
-cd 'test'
-npm install
-sleep 15
-node ./node_modules/protractor/bin/protractor conf.js
+`cd test`
+`npm install`
+`sleep 15`
+`node ./node_modules/protractor/bin/protractor conf.js`

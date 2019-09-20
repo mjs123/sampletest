4cbb6d1c391c3373322578d31b476071c18ff7e6
jayasankar <jayasankar.m@gmail.com>
2019-09-20 11:42:58 +0530

    updated conf file

--- test/conf.js
+++ test/conf.js
@@ -22,7 +22,8 @@ exports.config = {
 
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
-
+  allScriptsTimeout: 610000,
+  getPageTimeout: 310000,
   // Spec patterns are relative to the current working directory when
   // protractor is called.
   specs: [
@@ -34,6 +35,6 @@ exports.config = {
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 300000
   }
 };
